53f770c866c0a44fc7c7ae2cf78fe7d8c02efb43
Chihiro88G <1114chihiro@gmail.com>
2024-06-08 10:06:57 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("----- IntelliJ and Java Recap -----");
+        System.out.println("JDK: Java Development Kit. A software package that contains libraries and tools" +
+                "for developing and testing Java apps, and for running apps on Java platforms.");
+        System.out.println("Build systems: ");
+        System.out.println("1. IntelliJ: for plain and vanilla build usually for simple projects.");
+        System.out.println("2. Maven: One of industry standards. " +
+                "A popular open-source build tool developed by the Apache Group " +
+                "to build, publish, and deploy several projects at once for better project management. " +
+                "The tool provides allows developers to build and document the lifecycle framework."
+                );
+        System.out.println("3. Gradle: Another industry standard. " +
+                "An open-source build automation system that supports Maven and Ivy repositories for declaring the configuration of a project. " +
+                "Lots of open-source projects such as Spring are using it now. It is faster than Maven for most scenarios, thanks to its incremental builds");
+
+        git init, git add -A,
+    }
+}
